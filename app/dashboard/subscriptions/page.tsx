d239a39dae00fc6d4cd81cf53ba84e73362ace3e
François Noel <francoisnoel62@gmail.com>
2025-01-24 16:26:34 +0100

    refactor: remove unused imports from SubscriptionsPage component to streamline code and improve maintainability

--- app/dashboard/subscriptions/page.tsx
+++ app/dashboard/subscriptions/page.tsx
@@ -1,10 +1,6 @@
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from "@/app/lib/data";
-import { Suspense } from 'react';
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { formatDateToLocalFrance } from '@/app/lib/utils';
 
 export default async function SubscriptionsPage() {
