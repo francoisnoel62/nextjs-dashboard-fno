9fa37121287512f7acc40aad706db9f0a98ae7f9
François Noel <francoisnoel62@gmail.com>
2025-01-28 22:23:31 +0100

    fix: update email condition for rendering PrintAttendees and CreateClass components

--- app/dashboard/classes/page.tsx
+++ app/dashboard/classes/page.tsx
@@ -31,7 +31,7 @@ export default async function Page(
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
                 <Search placeholder="Search classes..."/>
-                {user && user.email === 'info@denzali.ch' && <CreateClass />}
+                {user && user.email === process.env.DZ_EMAIL && <CreateClass />}
             </div>
             <Suspense key={query + currentPage} fallback={<ClassesTableSkeleton/>}>
                 <Table query={query} currentPage={currentPage}/>
